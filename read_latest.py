04f70c74d6f834049c8d111ecf3f46426d53abb3
Manuel-Mockus <36791572+Manuel-Mockus@users.noreply.github.com>
2019-02-15 10:11:34 +0100

    Update read_latest.py

--- read_latest.py
+++ read_latest.py
@@ -22,39 +22,6 @@ nimg.show()
 for i in range(len(I)):
     print(len(I[i]))
 
-    '''
-
-
-#data is the full array of all digits, ratio = 0.7 is 70% training, 30% test
-def divisionAleatoire(data,ratio):
-    #TO ADD if ratio not in [0,1] error
-    dataP = data.copy()
-    P1 = [[]for i in range(10)]
-    P2 = [[]for i in range(10)]
-    P3 = [[]for i in range(10)]
-    P4 = [[]for i in range(10)]
-    
-    for i in range(len(dataP)):
-        normR = int(ratio*len(dataP[i]))
-        #random.shuffle(dataP[i])
-        n = int(len(dataP[i])/4)
-        P = [dataP[i][j:j+n] for j in range(0,len(dataP[i]),n)]
-        random.shuffle(P)
-        P1[i] = P[0][:normR]
-        P2[i] = P[1][:normR]
-        P3[i] = P[2][:normR]
-        P4[i] = P[3][:normR]
-
-
-    return P1,T1,P2,T2,P3,T3,P4,T4
-
-P1,T1,P2,T2,P3,T3,P4,T4 = divisionAleatoire(I,0.7)
-
-img = Image.new('L',(28,28))
-img.putdata(P1[2][32])
-nimg = img.resize((280,280))
-nimg.show()
-'''
 
 def centroids(Training):
     L = [np.zeros(28*28) for i in range(10)]
